eddfe520fdeb8d31bc7f458703dd605623587ba9
Krisha Arya <krisha.arya@globallogic.com>
2026-07-22 16:51:14 +0530

    test polling

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Jenkins CI/CD Pipeline!");
+    res.send("Hello from Jenkins CI/CD Pipeline - Version 2");
 });
 
 app.get("/health", (req, res) => {
